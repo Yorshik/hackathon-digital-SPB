6112353026ccb5a72d95d7d41a2696e310c594a4
Алексей <qu4rtz43@gmail.com>
2024-04-20 14:52:22 +0300

    add schedule test

---
+++ test_schedule.py
@@ -0,0 +1,9 @@
+import requests
+
+url = "https://digital.etu.ru/api/mobile/schedule?groupNumber=0304"
+
+page = requests.get(url)
+print(page.status_code)
+print(page.text)
+with open("result1.json", "w", encoding="utf8") as file:
+    file.write(page.text)
