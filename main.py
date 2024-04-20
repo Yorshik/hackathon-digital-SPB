aeb8a40d1e99b36113020a2d789888fc867a623a
Никита <107890115+caffreyfizz@users.noreply.github.com>
2024-04-20 14:38:25 +0300

    Added handler vuz.html

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ def index():
     return render_template('index.html')
 
 
+
+@app.route('/VUZ')
+def VUZ():
+    return render_template('VUZ.html')
+
 if __name__ == '__main__':
     app.run('localhost', 1234)
