9a79a8a6422b5943f7ba0688b3eec08a69a4c2a8
Алексей <qu4rtz43@gmail.com>
2024-04-21 00:22:37 +0300

    add schedule parser

--- test_schedule.py
+++ test_schedule.py
@@ -1,9 +1,34 @@
+import json
 import requests
 
-url = "https://digital.etu.ru/api/mobile/schedule?groupNumber=2181"
 
-page = requests.get(url)
-print(page.status_code)
-print(page.text)
-with open("result1.json", "w", encoding="utf8") as file:
-    file.write(page.text)
+def get_schedule(group_number=2181):
+    url = "https://digital.etu.ru/api/mobile/schedule"
+    params = {
+        "groupNumber": group_number
+    }
+
+    page = requests.get(url, params=params)
+    result_json = json.loads(page.text)["2181"]["days"]
+    all_days = {}
+    for day in result_json.values():
+        schedule = []
+        repetitions = []
+        for lesson in day["lessons"]:
+            if lesson["start_time"] in repetitions:
+                continue
+            else:
+                repetitions.append(lesson["start_time"])
+            lesson = {
+                "title": lesson["name"],
+                "start": lesson["start_time"],
+                "end": lesson["end_time"],
+                "housing": lesson["room"][0] if lesson["room"] else None
+            }
+            schedule.append(lesson)
+        all_days[day["name"]] = schedule
+    return all_days
+
+
+if __name__ == "__main__":
+    get_schedule()
