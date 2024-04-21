a7f31f77fd8b8f5ea184a63e7dcf55877055ab05
George Bartolomey <george@bh4.ru>
2024-04-21 15:13:23 +0300

    Rename test to schedule

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-params_for_geocode = {"geocode": "пр. Кузнецова 22к1", "lang": "ru_RU", "format": "json"}
-
-response = requests.get("https://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b", params=params_for_geocode).json()
-
-coords = response["response"]["GeoObjectCollection"]['featureMember'][0]['GeoObject']["Point"]
